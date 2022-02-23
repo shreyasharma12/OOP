f7466a8862f59a1ead312971528fe95c2309190d
Shreya Sharma <shreya_sharma1@baylor.edu>
2022-02-23 15:31:15 -0600

    class 2/23

--- CellPhoneClass.py
+++ CellPhoneClass.py
@@ -18,27 +18,21 @@ class CellPhone:
     def set_model(self, model):
         self.__manufact = model
 
-    def set_retail_price(self, manufact):
+    def set_retail_price(self, price):
         self.__retail_price = price
     
 ############################################################################
 
     def get_manufact(self):
         return self.__manufact
+
     def get_model(self):
         return self.__model
+
     def get_retail_price(self):
         return self.__retail_price
 
 
-
-
-
-    def get_retail_price(self):
-            return self.__retail_price
-
-    def__str__(self)
-    
 ############################################################################
     def __str__(self):
-        return 'The phone manufacturer is:' + str(self.__manufacturer) + "The phone model is:" + str(self.__model) + "The phone price is:" + format(self.__retail_price, ',.2f')
+        return 'The phone manufacturer is:' + self.__manufacturer + "The phone model is:" + self.__model + "The phone price is:" + format(self.__retail_price, ',.2f')
--- CellPhoneProgram.py
+++ CellPhoneProgram.py
@@ -13,8 +13,7 @@ def main():
 
     print("This is the car manufacturer:",phone.get_manufact())
     print("This is the car model:",phone.get_model())
-    print("This is the car retail price:",phone.get_retail_price
-    ())
-
+    print("This is the car retail price:",phone.get_retail_price())
+    print("Retail price: $", format(phone.get_retail_price()))
 
 main()
